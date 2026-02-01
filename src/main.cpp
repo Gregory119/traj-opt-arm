9984ce4c1ac680918e446025463ef85babae88cf
Gregory Jones <greggie119gj@gmail.com>
2026-02-01 16:08:07 -0500

    test multi-element trajectory

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include "simulator.hpp"
 
 // main function
-int main(int argc, const char** argv) {
-  // check command-line arguments
-  // if (argc!=2) {
-  //   std::printf(" USAGE:  basic modelfile\n");
-  //   return EXIT_FAILURE;
-  // }
-
-    Simulator::getInstance()->setTrajectory({{.time=3.0, .val={1.0, 0.0, 0.0, 0.0, 0.0, 0.0}}});
+int main(int argc, const char **argv)
+{
+    std::deque<TrajElement> traj{
+        {.time = 3.0, .val = {1.0, 0.0, 0.0, 0.0, 0.0, 0.0}},
+        {.time = 4.0, .val = {1.0, -1.0, 0.0, 0.0, 0.0, 0.0}},
+    };
+    Simulator::getInstance()->setTrajectory(std::move(traj));
     Simulator::getInstance()->run();
-    
-  return EXIT_SUCCESS;
+
+    return EXIT_SUCCESS;
 }
