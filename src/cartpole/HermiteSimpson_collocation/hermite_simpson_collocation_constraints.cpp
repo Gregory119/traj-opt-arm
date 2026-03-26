d546fdf563e9cdea79b63d35f1317c10e8ee2df1
Nate <npbernal@wpi.edu>
2026-03-25 18:56:48 -0700

    address comment 17 out of 17

--- src/cartpole/HermiteSimpson_collocation/hermite_simpson_collocation_constraints.cpp
+++ src/cartpole/HermiteSimpson_collocation/hermite_simpson_collocation_constraints.cpp
@@ -442,7 +442,7 @@ void SimpsonDefectConstraints::FillJacobianWrt(
     // insertions when constructing the final sparse jacobian matrix
     std::vector<Eigen::Triplet<double>> triplet_list;
     const int var_type_len = getVarTypeLen(var_type);
-    const int num_nonzero_submatrices = 1;
+    const int num_nonzero_submatrices = 2;
     const int num_defect_vec_eqns = m_num_segments;
     triplet_list.reserve(m_state_len * var_type_len * num_nonzero_submatrices
                          * num_defect_vec_eqns);
