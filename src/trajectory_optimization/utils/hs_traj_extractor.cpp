3e50e84a1420d830bba0e6a75eba34ca39ef8b76
Gregory Jones <greggie119gj@gmail.com>
2026-04-19 19:55:05 -0400

    add comments to trajectory extractor

--- src/trajectory_optimization/utils/hs_traj_extractor.cpp
+++ src/trajectory_optimization/utils/hs_traj_extractor.cpp
@@ -34,6 +34,12 @@ HermSimpTrajExtractor::HermSimpTrajExtractor(
 DiscreteJointStateTraj HermSimpTrajExtractor::createCollocationStateTraj(
     const pin::Model &model)
 {
+    /*
+    Loop through the state and midpoint state variables,
+    but handle the last state variable outside the loop.
+    This is because there is one less midpoint state variable
+    compared to the state variables at the knot points.
+    */
     DiscreteJointStateTraj traj;
     const int num_segments = m_state_mid_vars.size() / m_state_len;
 
@@ -79,6 +85,12 @@ DiscreteJointStateTraj HermSimpTrajExtractor::createCollocationStateTraj(
 DiscreteJointDataTraj HermSimpTrajExtractor::createCollocationCtrlTraj(
     const pinocchio::Model &model)
 {
+    /*
+    Loop through the state and midpoint state variables,
+    but handle the last state variable outside the loop.
+    This is because there is one less midpoint state variable
+    compared to the state variables at the knot points.
+    */
     DiscreteJointDataTraj traj;
     const int num_segments = m_ctrl_mid_vars.size() / m_ctrl_len;
 
